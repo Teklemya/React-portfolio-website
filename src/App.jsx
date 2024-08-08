1041ececdfca09a678c7c4ecad4ea42aa0d33b40
Teklemya <Teklemya@mail.uc.edu>
2024-08-07 20:29:18 -0400

    chore: Update eslint and package dependencies
    This commit updates the eslint configuration to disable the 'react/react-in-jsx-scope' rule. It also adds the 'three' package as a dependency in both the package-lock.json and package.json files. Additionally, it modifies the App.jsx file to include the BrowserRouter component and update the structure of the JSX elements. The About.jsx file is modified to remove the unused React import.
    
    Please review and merge this commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter } from 'react-router-dom';
+
+// eslint-disable-next-line no-unused-vars
+import { Hero, Navbar} from './components';
+
 const App = () => {
 
   return (
-    <>
-     <div>3D react fun project</div>
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-black">
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
+
   )
 }
 
